5707a2ecd01497e5716085f7d5538208e8f4b3f7
Alan Barbosa <alanbarbosadev@gmail.com>
2022-10-27 17:18:27 -0300

    Models Created

---
+++ EmployeesAPI/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeesAPI.Models
+{
+    public class Department
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+
+        public Department()
+        {
+            Id = Guid.NewGuid();
+            Employees = new List<Employee>();
+        }
+    }
+}
---
+++ EmployeesAPI/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeesAPI.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public double Salary { get; set; }
+        public DateTime Birthday { get; set; }
+        public Guid DepartmentId { get; set; }
+        public Department? Department { get; set; }
+        
+        public Employee()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
